712830f70977ec0163f220b945194401a0e37ea7
Marcin Pastwa <marcinpastwa@wp.pl>
2018-11-16 09:41:06 +0100

    main controller endpoint changed

--- src/main/java/com/monmar/personalbudget/controller/MainAppController.java
+++ src/main/java/com/monmar/personalbudget/controller/MainAppController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainAppController {
 
-    @GetMapping("/")
+    @GetMapping("/whereismymoney")
     public String showHomePage(Model model){
        return "index";
     }
